f40bc6f85e638bc095ab2f91cae7a921eb2398ef
nuttyproducer <benjamin.vanderzeypen@gmail.com>
2025-04-08 12:47:52 +0200

    Copy src/lib/toast.ts from firewatcher-alerts

---
+++ src/lib/toast.ts
@@ -0,0 +1,6 @@
+
+// This file re-exports toast functions from sonner for use throughout the app
+import { toast as sonnerToast } from "sonner";
+
+// Re-export the toast function for use in components
+export const toast = sonnerToast;
